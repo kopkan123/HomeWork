2acbc97302c773c6ec6fcdc8a7503e82b67f1106
Mikail <mikailrasulov674@gmail.com>
2023-02-04 13:44:41 +0300

    faf

---
+++ VseDomashi6/Program.cs
@@ -0,0 +1,135 @@
+﻿Console.WriteLine("Введите номер одной из трех домашних: ");
+int caseess = Convert.ToInt32(Console.ReadLine());
+switch(caseess){
+case 1:
+Console.WriteLine("введите количество строк");
+int chislo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int chislo2 = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[chislo, chislo2];
+FillArra(numbers);
+PrintAr(numbers);
+
+void FillArra(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintAr(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+break;
+
+case 2:
+Console.WriteLine("введите номер строки");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] number = new int [10,10];
+FillArray(number);
+
+if (n > number.GetLength(0) || m > number.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {number[n-1,m-1]}");
+}
+
+PrintArr(number);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
+break;
+
+case 3:
+Console.WriteLine("введите количество строк");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[,] numb = new int[c, b];
+FillArrayy(numb);
+
+
+for (int j = 0; j < numb.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numb.GetLength(0); i++)
+    {
+        avarage = (avarage + numb[i, j]);
+    }
+    avarage = avarage / c;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArrayy(numb);
+
+
+
+void FillArrayy(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArrayy(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+break;
+}
---
+++ VseDomashi7/Program.cs
@@ -0,0 +1,63 @@
+﻿Console.WriteLine("Введите номер одной из трех домашних: ");
+int caseess = Convert.ToInt32(Console.ReadLine());
+switch(caseess){
+case 1:
+Console.WriteLine("Введите натуральное число больше 1:");
+int number = int.Parse(Console.ReadLine());
+
+///Метод вывода натуральных чисел от N до 1:
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    NumberCounter (number - 1);
+}
+
+NumberCounter(number);
+break;
+
+case 2:
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
+break;
+
+case 3:
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"Функция Аккермана = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+break;
+}
